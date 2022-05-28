848ab9ca282955e9f78fb4177f7991ac846c30a5
GGCristo <cgg.code@gmail.com>
2022-05-28 12:43:09 +0100

    Revert "Foo commit just testing hooks"
    This reverts commit e31190b43afef6fed3ff0cd0b06319758d5db22b.

--- builtin/install/install.js
+++ builtin/install/install.js
@@ -1,5 +1,5 @@
 //@ts-check
-// TODO This file needs to be heavily tested
+// This file needs to be heavily tested
 import { config, updateJSON } from '../../config.cjs'
 import shell from "shelljs";
 import inquirer from 'inquirer';
